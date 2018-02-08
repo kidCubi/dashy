9d769a0353a5a0ab19f1144e900f86f331a0976c
yba <yassine.bakiri@kilokilo.ch>
2018-02-08 22:52:16 +0100

    Chnage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import css from './sass/App.scss';
+
+class App extends Component {
+    render() {
+        return (
+            <div className={css.App}>
+                <p>Hello</p>
+            </div>
+        );
+    }
+}
+
+export default App;

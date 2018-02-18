b159938aebd1f703536ce55f9e87b11d5cd0d066
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:40:47 +0100

    Add font loader

--- src/configFonts.js
+++ src/configFonts.js
@@ -3,7 +3,7 @@ const WebFont = require('webfontloader');
 export function LoadFonts() {
     const config = {
         google: {
-            families: ['Roboto:400, 500']
+            families: ['Roboto:100, 300']
         }
     };
     WebFont.load(config, 0.1, true);

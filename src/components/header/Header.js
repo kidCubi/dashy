83470d8583edd59ecf734f6447662b59835c3e6d
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:37:37 +0100

    Update Header module

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
+import styles from './Header.module.scss';
 
-class _COMPONENT extends Component {
-    constructor() {
-        super();
-        console.log('_COMPONENT')
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.triggerDrag = this.triggerDrag.bind(this);
+    }
+
+    componentDidMount() {
+    }
+
+    triggerDrag() {
+        (this.props.draggieInst.isEnabled) ? this.props.draggieInst.disable() : this.props.draggieInst.enable();
+        console.log(this.props.draggieInst.isEnabled)
     }
 
     render() {
         return (
-            <p> _COMPONENT </p>
+            <div className={styles.Wrapper}>
+                <p>Header</p>
+                <span className={styles.DragTrigger} onClick={this.triggerDrag}></span>
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+export default Header;

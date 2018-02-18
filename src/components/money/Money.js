8bd92109edad2c90c25e93c83f86c04f8fd1faf5
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:35:44 +0100

    Update Money Module

--- src/components/money/Money.js
+++ src/components/money/Money.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
+import styles from './Money.module.scss';
+import LineChart from './chart/LineChart';
 
-class _COMPONENT extends Component {
+import { connect } from 'react-redux';
+import { setMoneyLoaded } from '../../redux/actions/index'
+
+const mapState = state => ({
+    app: state.app,
+});
+
+const mapActions = dispatch => ({
+    setMoneyLoaded: (loaded) => dispatch(setMoneyLoaded(loaded))
+});
+
+class Money extends Component {
     constructor() {
         super();
-        console.log('_COMPONENT')
+        this.data = null;
+        this.totalBalance = 0;
+    }
+
+    componentDidMount() {
+        fetch("./assets/json/money.json")
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                this.data = data;
+                const amount = this.data[this.data.length - 1].amount;
+                this.totalBalance = (amount > 0) ? `+${amount}` : `-${amount}`;
+                this.props.setMoneyLoaded(true);
+            })
     }
 
     render() {
         return (
-            <p> _COMPONENT </p>
+            <div className={styles.Wrapper}>
+                <p> Money </p>
+                <span>{this.totalBalance} CHF</span>
+                {this.props.app.moneyLoaded &&
+                <LineChart
+                    data={this.data}
+                    width={600}
+                    height={300}
+                    id="chart_v1"
+                />
+                }
+                {this.props.children}
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+export default connect(mapState, mapActions)(Money);
+

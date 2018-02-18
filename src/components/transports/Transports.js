4041503ef7e3a6d3ed07a25ee4ab20ac17a9b40d
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:37:10 +0100

    Update Transports module

--- src/components/transports/Transports.js
+++ src/components/transports/Transports.js
@@ -1,16 +1,57 @@
 import React, { Component } from 'react';
+import TransportsEntry from './TransportsEntry';
+import styles from './Transports.module.scss';
 
-class _COMPONENT extends Component {
+import { connect } from 'react-redux';
+import { setTransportsLoaded } from '../../redux/actions/index'
+
+const mapState = state => ({
+    app: state.app,
+});
+
+const mapActions = dispatch => ({
+    setTransportsLoaded: (loaded) => dispatch(setTransportsLoaded(loaded))
+});
+
+class Transports extends Component {
     constructor() {
         super();
-        console.log('_COMPONENT')
+        this.indents = [];
+    }
+
+    componentDidMount() {
+        fetch("http://transport.opendata.ch/v1/stationboard?station=Erlenbach&limit=4")
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                for (let i = 0; i < data.stationboard.length; i++) {
+                    this.indents.push(<TransportsEntry
+                        number={data.stationboard[i].category + data.stationboard[i].number}
+                        destination={data.stationboard[i].to}
+                        departure={data.stationboard[i].stop.departureTimestamp}
+                        key={i}
+                    />)
+                }
+                this.props.setTransportsLoaded(true);
+            })
+
     }
 
     render() {
+        //Note : disons que dans le backend, l'utilisateur aura sélectionné 4 stations favorites
         return (
-            <p> _COMPONENT </p>
+            <div className={styles.Wrapper}>
+                {this.props.app.transportsLoaded &&
+                <div>
+                    {this.indents}
+                </div>
+                }
+                {this.props.children}
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+export default connect(mapState, mapActions)(Transports);
+
--- src/components/transports/TransportsEntry.js
+++ src/components/transports/TransportsEntry.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class _COMPONENT extends Component {
-    constructor() {
-        super();
-        console.log('_COMPONENT')
-    }
+const TransportsEntry = (props) => {
 
-    render() {
-        return (
-            <p> _COMPONENT </p>
-        );
-    }
-}
+    const departureMinutes = "0" + new Date(props.departure * 1000).getMinutes();
+    const dateNowMinutes = "0" + new Date(new Date().getTime() / 1000 * 1000).getMinutes();
+    departureMinutes.substr(-2);
+    dateNowMinutes.substr(-2);
+    const result = (departureMinutes - dateNowMinutes < 0) ? 60 + (departureMinutes - dateNowMinutes) : (departureMinutes - dateNowMinutes);
 
-export default _COMPONENT;
+    return (
+        <div>
+            <span>{props.number}</span>
+            <span>{props.destination}</span>
+            <span>{result}min</span>
+        </div>
+    );
+};
+
+export default TransportsEntry;

6b2eac8cedb77e63129edae5d54ad94ae1703980
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 23:56:40 +0100

    # ----------------------------------- # If applied, this commit will... # # Summarize changes in around 50 characters or less # Explain why this change is being made # # More detailed explanatory text, if necessary. Wrap it to about 72 # # characters or so. # # Explain the problem that this commit is solving. Focus on why you # # are making this change as opposed to how (the code explains that). # # Are there side effects or other unintuitive consequences of this # # change? Here's the place to explain them. # Provide links to any relevant tickets, articles or other resources # # Resolves: #123 # # See also: #456, #789 # -----------------------------------Update Loading module

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import styles from './Loading.module.scss';
 
-class _COMPONENT extends Component {
-    constructor() {
-        super();
-        console.log('_COMPONENT')
+
+class Loading extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingDone: false
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.hasLoaded) {
+            setTimeout(() => {
+                this.refLoading.style.display = "none";
+            }, 900);
+        }
     }
 
     render() {
+        const classes = classNames(
+            styles.Wrapper,
+            { [styles.HasLoaded]: this.props.hasLoaded },
+            { [styles.LoadingDone]: this.props.loadingDone }
+        );
         return (
-            <p> _COMPONENT </p>
+            <div className={classes} ref={(node) => {this.refLoading = node}} >
+                <span className={styles.Logo}></span>
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+export default Loading;

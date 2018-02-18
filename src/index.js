5515a9aec5d39964e1e69ad671392ae9562788d2
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:39:56 +0100

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import { Provider } from 'react-redux';
+import { LoadFonts } from "./configFonts";
+import configureStore from './redux/store/configureStore';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore();
+
+if (window.addEventListener)
+    window.addEventListener("load", startApp, false);
+else if (window.attachEvent)
+    window.attachEvent("onload", startApp);
+else window.onload = startApp;
+
+function startApp() {
+    LoadFonts();
+    ReactDOM.render(
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        document.getElementById('root')
+    );
+    registerServiceWorker();
+}

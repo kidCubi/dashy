099f12d89359d03bfc581e90aaf736c65bf97270
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:37:50 +0100

    Update TodoList module

--- src/components/todoList/TodoList.js
+++ src/components/todoList/TodoList.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
+import TodoListItem from './TodoListItem';
+import styles from './TodoList.module.scss';
 
-class _COMPONENT extends Component {
+import { connect } from 'react-redux';
+import { setTodoLoaded } from '../../redux/actions/index'
+
+const mapState = state => ({
+    app: state.app,
+});
+
+const mapActions = dispatch => ({
+    setTodoLoaded: (loaded) => dispatch(setTodoLoaded(loaded))
+});
+
+class TodoList extends Component {
     constructor() {
         super();
-        console.log('_COMPONENT')
+        this.todoItems = [
+            { value: "do stuff", complete: false },
+            { value: "do stuff", complete: false },
+            { value: "do stuff", complete: false },
+            { value: "do stuff", complete: false },
+        ];
+        this.state = {
+            count: this.todoItems.filter( s => { return !s.complete; }).length,
+        }
+    }
+
+    componentDidMount() {
+        this.props.setTodoLoaded(true);
+    }
+
+    checkItem(index) {
+        this.todoItems[index].complete = !this.todoItems[index].complete;
+        this.setState(state => ({
+            count: this.todoItems.filter( s => { return !s.complete; }).length
+        }));
     }
 
     render() {
+        const items = this.todoItems.map((item, index) => {
+            return (
+                <TodoListItem key={index} item={item} index={index} checkItem={this.checkItem.bind(this)}/>
+            );
+        });
         return (
-            <p> _COMPONENT </p>
+            <div className={styles.Wrapper}>
+                <span>{this.state.count}</span>
+                {this.props.app.todoLoaded &&
+                <ul className="list-group"> {items} </ul>
+                }
+                {this.props.children}
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+export default connect(mapState, mapActions)(TodoList);
+
--- src/components/todoList/TodoListItem.js
+++ src/components/todoList/TodoListItem.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
+import styles from './TodoList.module.scss';
 
-class _COMPONENT extends Component {
-    constructor() {
-        super();
-        console.log('_COMPONENT')
+class TodoListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.checkTask = this.checkTask.bind(this);
+    }
+
+    checkTask() {
+        this.props.checkItem(this.props.index);
     }
 
     render() {
+        const classes = classNames(
+            { [styles.isChecked]: this.props.item.done },
+        );
         return (
-            <p> _COMPONENT </p>
+            <li className={classes}>
+                <div className={this.props.item.done ? "done" : "undone"}>
+                    <span aria-hidden="true" onClick={this.checkTask}></span>
+                    {this.props.item.value}
+                </div>
+            </li>
         );
     }
 }
 
-export default _COMPONENT;
+export default TodoListItem;

8c8744c78a7418e810f3be4dbfee3c15b78ee6a7
yba <yassine.bakiri@kilokilo.ch>
2018-02-18 14:37:24 +0100

    Update Agenda module

--- src/components/agenda/Agenda.js
+++ src/components/agenda/Agenda.js
@@ -1,16 +1,61 @@
 import React, { Component } from 'react';
+import styles from './Agenda.module.scss';
 
-class _COMPONENT extends Component {
+import AgendaEntry from "./AgendaEntry";
+
+import { connect } from 'react-redux';
+import { setAgendaLoaded } from '../../redux/actions/index'
+
+const mapState = state => ({
+    app: state.app,
+});
+
+const mapActions = dispatch => ({
+    setAgendaLoaded: (loaded) => dispatch(setAgendaLoaded(loaded))
+});
+
+class Agenda extends Component {
     constructor() {
         super();
-        console.log('_COMPONENT')
+        this.today = new Date().getUTCDate();
+        this.indents = [];
+    }
+
+    componentDidMount() {
+        fetch("./assets/json/agenda.json")
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                for (let i = 0; i < data.length; i++) {
+                    this.indents.push(<AgendaEntry
+                        eventName={data[i].title}
+                        hour={data[i].hour}
+                        label={data[i].label}
+                        participants={data[i].participants}
+                        key={i}
+                    />);
+                }
+                this.props.setAgendaLoaded(true);
+            })
     }
 
     render() {
         return (
-            <p> _COMPONENT </p>
+            <div className={styles.Wrapper}>
+                <div className={styles.Heading}>
+                    <span className={styles.IconToday}>{this.today}</span>
+                    <span className={styles.Title}>Today</span>
+                </div>
+                {this.props.app.agendaLoaded &&
+                <div className={styles.Entries}>
+                    {this.indents}
+                </div>
+                }
+                {this.props.children}
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+export default connect(mapState, mapActions)(Agenda);
--- src/components/agenda/AgendaEntry.js
+++ src/components/agenda/AgendaEntry.js
@@ -1,16 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
+import classNames from 'classnames';
+import styles from './Agenda.module.scss';
 
-class _COMPONENT extends Component {
-    constructor() {
-        super();
-        console.log('_COMPONENT')
+const AgendaEntry = (props) => {
+    let classPriority = "";
+    switch (props.label) {
+        case "daily":
+            classPriority = styles.Daily;
+            break;
+        case "normal":
+            classPriority = styles.Normal;
+            break;
+        case "urgent":
+            classPriority = styles.Urgent;
+            break;
+        default:
+            classPriority = ""
     }
 
-    render() {
-        return (
-            <p> _COMPONENT </p>
-        );
-    }
-}
+    const classes = classNames(
+        classPriority,
+        styles.Entry
+    );
 
-export default _COMPONENT;
+    let participantsArr = [];
+    let leftParticipants = null;
+    if (props.participants.length >= 3) {
+        participantsArr = props.participants.slice(0, 2).map((value, index) => {
+            leftParticipants = <li className={styles.LeftParticipants}>+{props.participants.length - 2}</li>;
+            return (
+                <li
+                    className={styles.Participant}
+                    style={{ backgroundImage: `url(${value})` }}
+                    key={index}
+                />
+            )
+        });
+    } else {
+        participantsArr = props.participants.map((value, index) => {
+            return (
+                <li
+                    className={styles.Participant}
+                    style={{ backgroundImage: `url(${value})` }}
+                    key={index}
+                />
+            )
+        })
+    }
+
+    return (
+        <div className={classes}>
+            <span className={styles.Hour}>{props.hour}</span>
+            <ul className={styles.Participants}>
+                {leftParticipants}
+                {participantsArr}
+            </ul>
+            <span className={styles.EventName}>{props.eventName}</span>
+        </div>
+    );
+};
+
+export default AgendaEntry;

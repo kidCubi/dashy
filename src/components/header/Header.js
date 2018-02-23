fb49fbabc563bb4bbfa5a5a310fed43cf2cd947f
yba <yassine.bakiri@kilokilo.ch>
2018-02-23 22:33:48 +0100

    Update Header module

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
-//TODO : résoudre bug sur draggie
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './Header.module.scss';
+
+import { connect } from 'react-redux';
+import { setMenuOverlayCoordinates, setMenuOpenClose } from '../../redux/actions/index'
+
+const mapState = state => ({
+    app: state.app,
+});
+
+const mapActions = dispatch => ({
+    setMenuOverlayCoordinates: (x, y) => dispatch(setMenuOverlayCoordinates(x, y)),
+    setMenuOpenClose: (opened) => dispatch(setMenuOpenClose(opened))
+});
 
 class Header extends Component {
     constructor(props) {
         super(props);
-        this.triggerDrag = this.triggerDrag.bind(this);
+        this.displayMenu = this.displayMenu.bind(this)
     }
 
-    triggerDrag() {
-        // (this.props.draggieInst.isEnabled) ? this.props.draggieInst.disable() : this.props.draggieInst.enable();
-        console.log(this.props.draggieInst.isEnabled)
+    displayMenu(e) {
+        const x = e.clientX;
+        const y = e.clientY;
+        this.props.setMenuOverlayCoordinates(x, y);
+        this.props.setMenuOpenClose(true);
     }
 
     render() {
         return (
-            <div className={styles.Wrapper}>
-                <span className={styles.HeaderMenu}></span>
-                <span className={styles.HeaderIcon}></span>
-                <span className={styles.DragTrigger} onClick={this.triggerDrag}></span>
+            <div>
+                <div className={styles.Wrapper}>
+                    <span className={styles.HeaderMenu}/>
+                    <span className={styles.HeaderIcon}/>
+                    <span className={styles.DragTrigger} onClick={this.displayMenu}/>
+                </div>
+                {this.props.children}
             </div>
         );
     }
 }
 
 Header.propTypes = {
-    //TODO : résoudre bug sur draggie
-   // draggieInst:
+    children: PropTypes.array
 };
 
-
-export default Header;
+export default connect(mapState, mapActions)(Header);

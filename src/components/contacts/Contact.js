bb66f36a828f8a659398971a434eb15b9542d67f
yba <yassine.bakiri@kilokilo.ch>
2018-02-25 23:36:13 +0100

    Update contact module

---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styles from './Contacts.module.scss';
+
+const Contact = (props) => {
+    return (
+        <div className={styles.ContactItem}>
+            <span className={styles.Avatar}
+                  style={{ backgroundImage: `url(${props.avatar})` }}
+            />
+            <div className={styles.ContactInfos}>
+                <span className={styles.Name}>{props.name}</span>
+                <div className={styles.ContactLinks}>
+                    <a href={`tel:${props.tel}`} className={styles.Telephone}></a>
+                    <a href={`sms:${props.tel}`} className={styles.Text}></a>
+                    <a href={`mailto:${props.email}`} className={styles.Email}></a>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+Contact.propTypes = {
+    name: PropTypes.string,
+    tel: PropTypes.string,
+    email: PropTypes.string,
+    avatar: PropTypes.string,
+};
+
+export default Contact;
--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Contact from './Contact';
+import styles from './Contacts.module.scss';
 
-class _COMPONENT extends Component {
+import { connect } from 'react-redux';
+import { setContactsLoaded } from '../../redux/actions/index'
+
+const mapState = state => ({
+    app: state.app,
+});
+
+const mapActions = dispatch => ({
+    setContactsLoaded: (loaded) => dispatch(setContactsLoaded(loaded))
+});
+
+class Contacts extends Component {
     constructor() {
         super();
-        console.log('_COMPONENT')
+        this.indents = [];
+    }
+
+    componentDidMount() {
+        fetch("./assets/json/contacts.json")
+            .then((response) => {
+                return response.json()
+            })
+            .then((data) => {
+                for (let i = 0; i < data.length; i++) {
+                    this.indents.push(<Contact
+                        name={data[i].name.substring(0, 17)}
+                        tel={data[i].tel}
+                        email={data[i].email}
+                        avatar={data[i].avatar}
+                        key={i}
+                    />);
+                }
+                this.props.setContactsLoaded(true);
+            });
     }
 
     render() {
         return (
-            <p> _COMPONENT </p>
+            <div className={styles.Wrapper}>
+                {this.props.app.modulesLoaded.contactsLoaded &&
+                <div className={styles.Contact}>
+                    {this.indents}
+                </div>
+                }
+            </div>
         );
     }
 }
 
-export default _COMPONENT;
+Contacts.propTypes = {
+    children: PropTypes.array
+};
+
+export default connect(mapState, mapActions)(Contacts);
